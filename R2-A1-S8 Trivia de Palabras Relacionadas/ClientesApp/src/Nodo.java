59106f14fcfc09b047d048976cefb2937f71f292
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-04-21 10:30:46 -0500

    Create Nodo.java

---
+++ R2-A1-S8 Trivia de Palabras Relacionadas/ClientesApp/src/Nodo.java
@@ -0,0 +1,10 @@
+public class Nodo {
+    
+    Cliente cliente;
+    Nodo siguiente;
+
+    public Nodo(Cliente cliente) {
+        this.cliente = cliente;
+        this.siguiente = null;
+    }
+}
