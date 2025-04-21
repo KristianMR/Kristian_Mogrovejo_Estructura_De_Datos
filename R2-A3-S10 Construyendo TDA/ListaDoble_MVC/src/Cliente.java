1f8e2d9df01e2c0272ab2476ddbfbedaadefc218
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-04-21 11:11:17 -0500

    Create Cliente.java

---
+++ R2-A3-S10 Construyendo TDA/ListaDoble_MVC/src/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+    
+    private int cedula;
+    private String nombre;
+
+    public Cliente(int cedula, String nombre){
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public int getCedula(){
+        return cedula;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+}
