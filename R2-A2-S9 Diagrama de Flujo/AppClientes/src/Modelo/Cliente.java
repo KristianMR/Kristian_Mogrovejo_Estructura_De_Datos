f9052012d52afd897e467c07ba38e4977f146ada
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-04-24 13:54:48 -0500

    Create Cliente.java

---
+++ R2-A2-S9 Diagrama de Flujo/AppClientes/src/Modelo/Cliente.java
@@ -0,0 +1,25 @@
+package Modelo;
+
+public class Cliente {
+
+    private int cedula;
+    private String nombre;
+
+    public Cliente(int cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Cedula: " + cedula + ", Nombre: " + nombre;
+    }
+}
