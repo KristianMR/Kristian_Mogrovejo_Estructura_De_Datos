6f57747f755a8d62896208c1bbfea35fcfc40b9e
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-08 11:24:30 -0500

    Update ListaCircular.java

--- R3-A1-S12 Notebook/ListaCircular/src/Modelo/ListaCircular.java
+++ R3-A1-S12 Notebook/ListaCircular/src/Modelo/ListaCircular.java
@@ -1,5 +1,35 @@
 package Modelo;
 
 public class ListaCircular {
-    
+
+    private cliente inicio;
+
+    public void InsertarCliente(String nombre, String cedula){
+
+        cliente nuevo = new cliente(nombre, cedula);
+
+        if(inicio == null){
+            nuevo.siguiente = nuevo;
+            inicio = nuevo;
+        }else{
+            nuevo.siguiente = inicio.siguiente;
+            inicio.siguiente = nuevo;
+            inicio = nuevo;
+        }
+    }
+
+    public String ListarClientes(){
+
+        if(inicio == null) return "La lista está vacía.";
+
+        StringBuilder sb = new StringBuilder();
+        cliente actual = inicio.siguiente;
+
+        do{
+            sb.append("Nombre: ").append(actual.getNombre()).append(", Cedula: ").append(actual.getCedula()).append("\n");
+            actual = actual.siguiente;
+        }while(actual != inicio.siguiente);
+
+        return sb.toString();
+    }
 }
