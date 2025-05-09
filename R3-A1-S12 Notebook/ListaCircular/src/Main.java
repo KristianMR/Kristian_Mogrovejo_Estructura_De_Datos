a18ff2903dc5cc513a533a59e7e290d612adb451
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-08 23:51:44 -0500

    Add comments

--- R3-A1-S12 Notebook/ListaCircular/src/Main.java
+++ R3-A1-S12 Notebook/ListaCircular/src/Main.java
@@ -3,10 +3,19 @@ import Modelo.ListaCircular;
 import Vista.vista;
 
 public class Main {
+    //método principal que inicia la aplicación
     public static void main(String[] args) throws Exception {
-         ListaCircular modelo = new ListaCircular();
+
+        //crea una instancia del modelo que maneja la lista circular de clientes
+        ListaCircular modelo = new ListaCircular();
+
+        //crea una instancia de la vista que maneja la interfaz gráfica
         vista vista = new vista();
+
+        //crea una instancia del controlador que maneja la lógica de la aplicación
         controlador controlador = new controlador(modelo, vista);
+        
+        //inicia la aplicación llamando al método Iniciar del controlador
         controlador.Iniciar();
     }
 }
