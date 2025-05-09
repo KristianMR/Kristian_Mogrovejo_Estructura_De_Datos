c7de3691539cf984ab261047bcb1171a9572c4df
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-08 23:01:19 -0500

    Add comments

--- R3-A1-S12 Notebook/ListaCircular/src/Modelo/cliente.java
+++ R3-A1-S12 Notebook/ListaCircular/src/Modelo/cliente.java
@@ -4,8 +4,11 @@ public class cliente {
 
     private String nombre;
     private String cedula;
+    
+    //referencia al siguiente cliente en la lista circular
     cliente siguiente;
 
+    //constructor de la clase cliente que inicializa el nombre y la cedula del cliente
     public cliente(String nombre, String cedula){
 
         this.nombre = nombre;
@@ -13,10 +16,12 @@ public class cliente {
         this.siguiente = null;
     }
 
+    //metodo para obtener el nombre del cliente
     public String getNombre(){
         return nombre;
     }
     
+    //metodo para obtener la cedula del cliente
     public String getCedula(){
         return cedula;
     }
