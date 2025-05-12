0334a7d8aa0f1beac453b9f02261fea5575c2880
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-12 00:36:58 -0500

    Update Main.java

--- R3-A2-S14 Video Participativo/MetodosOrdenamiento/src/Main.java
+++ R3-A2-S14 Video Participativo/MetodosOrdenamiento/src/Main.java
@@ -1,5 +1,11 @@
+import Controlador.Controlador;
+import Vista.Vista;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Vista vista = new Vista();
+        Controlador controlador = new Controlador(vista);
+        Controlador.iniciar();
     }
 }
