deeb75dad5a46df7b2d08c914a418de6fa7a7d31
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-19 01:06:27 -0500

    Update App.java

--- R3-A4-S14 Push and Pop/PushAndPop/src/App.java
+++ R3-A4-S14 Push and Pop/PushAndPop/src/App.java
@@ -1,5 +1,8 @@
+import Controller.DequeController;
+import javax.swing.SwingUtilities;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SwingUtilities.invokeLater(DequeController::new);
     }
 }
