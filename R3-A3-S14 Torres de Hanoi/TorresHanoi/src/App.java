ea4286d0ec8d508a7632bb1043b2092d52ab234d
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-19 00:17:37 -0500

    Update App.java

--- R3-A3-S14 Torres de Hanoi/TorresHanoi/src/App.java
+++ R3-A3-S14 Torres de Hanoi/TorresHanoi/src/App.java
@@ -1,5 +1,16 @@
+import Controller.hanoiController;
+import Model.hanoiModel;
+import View.hanoiView;
+
+import javax.swing.SwingUtilities;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        hanoiModel model = new hanoiModel();
+        hanoiView view = new hanoiView();
+        hanoiController controller = new hanoiController(model, view);
+
+        SwingUtilities.invokeLater(controller::start);
     }
 }
