478b8c616011e1e06a6d61fc40206c607baf2272
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-25 22:33:58 -0500

    Update App.java

--- R3-A5-S15 Reforestando/ArbolesBinarios/src/App.java
+++ R3-A5-S15 Reforestando/ArbolesBinarios/src/App.java
@@ -1,5 +1,8 @@
+import Controlador.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Controlador controlador = new Controlador(null, null); // Crea una instancia del controlador
+        controlador.Iniciar(); // Inicia el controlador, que a su vez inicia la aplicación
     }
 }
